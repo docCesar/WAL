3ba9afcb3bc830967858531b2d9ba73d2881524f
docCesar <1105086063@qq.com>
2019-08-08 17:15:54 +0900

    Finished Hall factors

--- walAnalysis.m
+++ walAnalysis.m
@@ -3,7 +3,7 @@ clear;close all;clc
 % Constants
 h=6.6260699*10^-34;
 hbar=1.0545718e-34;
-m=9.10938356*10^(-31);
+me=9.10938356*10^(-31);
 e=1.60217662*10^-19;
 
 % Parameters of samples
@@ -21,12 +21,6 @@ fileName=dir(fullfile('*.txt'));
 number=length(fileName);
 % Claim variables.
 hall=[];
-bH=[];
-rH=[];
-gHall=[];
-bWAL=[];
-rWAL=[];
-gWAL=[];
 mu=[];
 taup=[];
 vf=[];
@@ -34,7 +28,6 @@ dif=[];
 be=[];
 ne=[];
 kf=[];
-
 bi=[];
 bso=[];
 directionForTag=string;
@@ -64,17 +57,19 @@ for i=1:number
         x(m(1)-1:end)=[];
         y(m(1)-1:end)=[];
     end
-    
+    x=x./1000;
     if direction=="x"
         eval(['hxx_',temperature{1},'K=x;'])
         eval(['rxx_',temperature{1},'K=y;'])
+        eval(['gxx_',temperature{1},'K=asp*h/(e^2)./y;'])
     elseif direction=="y"
         eval(['hxy_',temperature{1},'K=x;'])
         eval(['rxy_',temperature{1},'K=y;'])
+        eval(['gxy_',temperature{1},'K=asp*h/(e^2)./y;'])
     else
         "Something wrong with file name"
     end
-    clearvars x y m direction temperature
+    clearvars x y m direction temperature;
 end
 temperatureForTag(1)=[];
 temperatureForTag=strip(temperatureForTag,'left','0');
@@ -85,14 +80,69 @@ temperatureForCheck=convertStringsToChars(temperatureForCheck);
 figure
 for i=1:number/2
    eval(['plot(hxy_',temperatureForCheck{i},'K,rxy_',temperatureForCheck{i},'K,''Linewidth'',2)'])
-%    plot(bH(:,i),rH(:,i),'Linewidth',2)
    xlabel {H (T)}
    ylabel {R_{xy} (\Omega)}
    grid on
    hold on
     
 end
+title('Hall resistance')
+legend(temperatureForTag(1:number/2))
 
+%% Hall calibration
+for i=1:number/2
+    eval(['temR=rxy_',temperatureForCheck{i},'K;'])
+    temR=flipud(temR);
+    eval(['rxyCali_',temperatureForCheck{i},'K=(rxy_',temperatureForCheck{i},...
+        'K-temR)./2;'])
+end
+clearvars temR;
+
+%% Hall fitting
+for i=1:number/2
+    eval(['[xData, yData] = prepareCurveData( hxy_',temperatureForCheck{i},...
+        'K, rxyCali_',temperatureForCheck{i},'K);'])
+%     [xData, yData] = prepareCurveData( bH(:,i+number/2), rHN(:,i+number/2) );
+
+    % Set up fittype and options.
+    ft = fittype( 'p1*x+p2', 'independent', 'x', 'dependent', 'y' );
+    opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+    opts.Display = 'Off';
+    opts.Robust = 'Bisquare';
+    opts.StartPoint = [0.9134 0.0975404049994095];
+    opts.TolFun = 1e-08;
+    opts.TolX = 1e-08;
+
+    % Fit model to data.
+    [fitresult, gof] = fit( xData, yData, ft, opts );
+
+    % Extract fitting parameters.
+    fitresult.p1
+    hall(end+1)=abs(fitresult.p1)
+    
+    % Plot fit with data.
+    figure( 'Name', 'untitled fit 1' );
+    plotH = plot( fitresult, xData, yData );
+    legend( plotH, 'rH vs. bH', 'untitled fit 1', 'Location', 'NorthEast' );
+    % Label axes
+    xlabel H
+    ylabel {R_xy}
+    grid on
+
+    % Properties from Hall fitting.
+    ne(i)=abs(1/e/hall(i));
+    eval(['mu(i)=e^2/h*max(gxx_',temperatureForCheck{i},'K)/e/ne(i);'])
+%     mu(i)=e^2/h*max(gWAL(:,i))/e/ne(i);
+    taup(i)=me/e*mu(i);
+    kf(i)=(2*pi*ne(i))^(1/2);  % 2D case.
+    %kf(i)=(3*pi^2*ne)^(1/3);  % 3D case.
+    vf(i)=hbar/me*kf(i);
+    dif(i)=vf(i)^2*taup(i)/2;
+    be(i)=hbar/(4*e*dif(i)*taup(i));
+
+end
+'Hall finish'
+% return
 
 
 function [x,y] = importfile(filename, startRow, endRow)
